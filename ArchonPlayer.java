da3f898b7ec89e99672d5bec73598f60d080a7c0
jay <jay@jay-laptop.(none)>
2010-01-16 18:07:43 -0500

    fixed indentation in ArchonPlayer.java

--- ArchonPlayer.java
+++ ArchonPlayer.java
@@ -4,64 +4,64 @@ package team338;
 import battlecode.common.*;
 import static battlecode.common.GameConstants.*;
 
-public class ArchonPlayer implements BasePlayer 
+public class ArchonPlayer implements BasePlayer
 {
 
     private final RobotController myRC;
-    
-    public ArchonPlayer(RobotController rc) 
+
+    public ArchonPlayer(RobotController rc)
     {
         myRC = rc;
     }
 
-    public void run() 
+    public void run()
     {
         //System.out.println("STARTING");
-        while (true) 
+        while (true)
         {
-            try 
+            try
             {
                 /*** beginning of main loop ***/
-                while (myRC.isMovementActive()) 
+                while (myRC.isMovementActive())
                 {
                     myRC.yield();
                 }
                 Robot[] nbrs = myRC.senseNearbyGroundRobots();
-                        for(Robot r : nbrs)
+                for(Robot r : nbrs)
+                {
+
+                    RobotInfo info = myRC.senseRobotInfo(r);
+                    if(info.location.distanceSquaredTo(myRC.getLocation()) <= 2 &&
+                       info.type == RobotType.WOUT && info.team.equals(myRC.getTeam()))
+                    {
+                        double maxTransfer = info.maxEnergon - info.eventualEnergon;
+                        if(maxTransfer < myRC.getEnergonLevel())
                         {
-                                
-                                RobotInfo info = myRC.senseRobotInfo(r);
-                                if(info.location.distanceSquaredTo(myRC.getLocation()) <= 2 && 
-                                   info.type == RobotType.WOUT && info.team.equals(myRC.getTeam()))
-                                {
-                                    double maxTransfer = info.maxEnergon - info.eventualEnergon;
-                                    if(maxTransfer < myRC.getEnergonLevel())
-                                    {
-                                        myRC.transferUnitEnergon(1, info.location, RobotLevel.ON_GROUND); 
-                                    }
-                                }
+                            myRC.transferUnitEnergon(1, info.location, RobotLevel.ON_GROUND);
                         }
-                        MapLocation spawnLoc  = myRC.getLocation().add(myRC.getDirection());
-                        if(myRC.getEnergonLevel() > RobotType.WOUT.spawnCost() && 
-                           myRC.senseTerrainTile(spawnLoc).getType() == TerrainTile.TerrainType.LAND && 
-                           myRC.senseGroundRobotAtLocation(spawnLoc) == null) 
-                        {
-                            myRC.spawn(RobotType.WOUT);
+                    }
+                }
+                MapLocation spawnLoc  = myRC.getLocation().add(myRC.getDirection());
+                if(myRC.getEnergonLevel() > RobotType.WOUT.spawnCost() &&
+                   myRC.senseTerrainTile(spawnLoc).getType() == TerrainTile.TerrainType.LAND &&
+                   myRC.senseGroundRobotAtLocation(spawnLoc) == null)
+                {
+                    myRC.spawn(RobotType.WOUT);
                         }
-                        else if (myRC.canMove(myRC.getDirection())) 
-                        {
-                            System.out.println("about to move");
-                            myRC.moveForward();
-                        } 
-                        else 
-                        {
-                            myRC.setDirection(myRC.getDirection().rotateRight());
-                        }
-                        myRC.yield();
-                        
-                        /*** end of main loop ***/
-            } 
-            catch (Exception e) 
+                else if (myRC.canMove(myRC.getDirection()))
+                {
+                    System.out.println("about to move");
+                    myRC.moveForward();
+                }
+                else
+                {
+                    myRC.setDirection(myRC.getDirection().rotateRight());
+                }
+                myRC.yield();
+
+                /*** end of main loop ***/
+            }
+            catch (Exception e)
             {
                 System.out.println("caught exception:");
                 e.printStackTrace();
