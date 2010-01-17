da405a2cf8c7ca982db76644121780fb7a8d180a
Adam Kidder <thekidder@gmail.com>
2010-01-17 16:17:34 -0500

    formatting fixes

--- BasePlayer.java
+++ BasePlayer.java
@@ -8,24 +8,34 @@ abstract class BasePlayer implements Runnable
     protected MapLocation location;
     protected Direction direction;
     protected final Team team;//safely assume this does not change
-    private BasePlayer(){
+
+    private BasePlayer()
+    {
         System.out.println("needs a RobotController");
         myRC = null;
         team = null;
     }
-    public BasePlayer(RobotController rc){
+
+    public BasePlayer(RobotController rc)
+    {
         myRC = rc;
         team = myRC.getTeam();
     }
+
     /**
      *runPlayer should return before the end of the turn and should not call yield
      *
      */
     public abstract void runPlayer() throws GameActionException;
-    public void run(){
-        while(true){
-            try{
-                while(true){
+
+    public void run()
+    {
+        while(true)
+        {
+            try
+            {
+                while(true)
+                {
                     location = myRC.getLocation();
                     direction = myRC.getDirection();
                     runPlayer();
