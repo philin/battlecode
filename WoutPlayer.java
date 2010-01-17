9b5e13ef80752cebd6a724e3a58f14e8d5c9c519
jay <jay@jay-laptop.(none)>
2010-01-16 20:53:49 -0500

    follow an Archon when energon is low

--- WoutPlayer.java
+++ WoutPlayer.java
@@ -6,10 +6,23 @@ import static battlecode.common.GameConstants.*;
 public class WoutPlayer implements BasePlayer
 {
     private final RobotController myRC;
+    private static final double ARCHON_FIND_THRESHOLD=10;
 
     public WoutPlayer(RobotController rc)
     {
         myRC = rc;
+    }
+
+    public void tryMoveForward() throws GameActionException
+    {
+        if(myRC.canMove(myRC.getDirection()))
+        {
+            System.out.println("about to move");
+            myRC.moveForward();
+        }
+        else{
+            myRC.setDirection(myRC.getDirection().rotateRight());
+        }
     }
 
     public void run()
@@ -23,20 +36,56 @@ public class WoutPlayer implements BasePlayer
                 {
                     myRC.yield();
                 }
-
-                if (myRC.canMove(myRC.getDirection()))
+                if(myRC.getEnergonLevel()<ARCHON_FIND_THRESHOLD)
                 {
-                    System.out.println("about to move");
-                    myRC.moveForward();
-                } 
+                    MapLocation loc = myRC.getLocation();
+                    MapLocation minArch = null;
+                    int minDistSquared=-1;
+                    for(MapLocation arch: myRC.senseAlliedArchons())
+                    {
+                        if(minArch==null)
+                        {
+                            minArch=arch;
+                            minDistSquared = arch.distanceSquaredTo(loc);
+                        }
+                        else
+                        {
+                            int dist = arch.distanceSquaredTo(loc);
+                            if(dist<minDistSquared){
+                                minDistSquared=dist;
+                                minArch=arch;
+                            }
+                        }
+                    }
+                    Direction currDir = myRC.getDirection();
+                    Direction dir = loc.directionTo(minArch);
+                    if(currDir==dir)
+                    {
+                        tryMoveForward();
+                    }
+                    else if(dir==Direction.OMNI)
+                    {
+                        //do noting
+                    }
+                    else
+                    {
+                        if(myRC.canMove(dir))
+                        {
+                            myRC.setDirection(dir);
+                        }
+                        else{
+                            tryMoveForward();
+                        }
+                    }
+                }
                 else
                 {
-                    myRC.setDirection(myRC.getDirection().rotateRight());
+                    tryMoveForward();
                 }
                 myRC.yield();
 
                 /*** end of main loop ***/
-            } 
+            }
             catch (Exception e)
             {
                 System.out.println("caught exception:");
