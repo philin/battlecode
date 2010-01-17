40c565f0028e3dd74bb83af0cba68299199583c2
Jay Yang <jkelleyy@gmail.com>
2010-01-17 11:12:15 -0500

    Wout player attacks things in range when possible

--- WoutPlayer.java
+++ WoutPlayer.java
@@ -6,11 +6,13 @@ import static battlecode.common.GameConstants.*;
 public class WoutPlayer implements BasePlayer
 {
     private final RobotController myRC;
+    private final Team team;//safely assume this does not change
     private static final double ARCHON_FIND_THRESHOLD=10;
 
     public WoutPlayer(RobotController rc)
     {
         myRC = rc;
+        team = rc.getTeam();
     }
 
     public void tryMoveForward() throws GameActionException
@@ -20,7 +22,8 @@ public class WoutPlayer implements BasePlayer
             System.out.println("about to move");
             myRC.moveForward();
         }
-        else{
+        else
+        {
             myRC.setDirection(myRC.getDirection().rotateRight());
         }
     }
@@ -31,56 +34,92 @@ public class WoutPlayer implements BasePlayer
         {
             try
             {
-                /*** beginning of main loop ***/
-                while (myRC.isMovementActive())
-                {
-                    myRC.yield();
-                }
-                if(myRC.getEnergonLevel()<ARCHON_FIND_THRESHOLD)
-                {
-                    MapLocation loc = myRC.getLocation();
-                    MapLocation minArch = null;
-                    int minDistSquared=-1;
-                    for(MapLocation arch: myRC.senseAlliedArchons())
+                MapLocation loc = myRC.getLocation();
+                Direction currDir = myRC.getDirection();
+                //if can move
+                if (!myRC.isMovementActive()){
+                    if(myRC.getEnergonLevel()<ARCHON_FIND_THRESHOLD)
                     {
-                        if(minArch==null)
+                        MapLocation minArch = null;
+                        int minDistSquared=-1;
+                        for(MapLocation arch: myRC.senseAlliedArchons())
                         {
-                            minArch=arch;
-                            minDistSquared = arch.distanceSquaredTo(loc);
+                            if(minArch==null)
+                            {
+                                minArch=arch;
+                                minDistSquared = arch.distanceSquaredTo(loc);
+                            }
+                            else
+                            {
+                                int dist = arch.distanceSquaredTo(loc);
+                                if(dist<minDistSquared){
+                                    minDistSquared=dist;
+                                    minArch=arch;
+                                }
+                            }
+                        }
+                        if(minArch==null){
+                            //we've lost
+                            myRC.yield();
+                        }
+
+                        Direction dir = loc.directionTo(minArch);
+                        if(currDir==dir)
+                        {
+                            tryMoveForward();
+                        }
+                        else if(dir==Direction.OMNI)
+                        {
+                            //do noting
                         }
                         else
                         {
-                            int dist = arch.distanceSquaredTo(loc);
-                            if(dist<minDistSquared){
-                                minDistSquared=dist;
-                                minArch=arch;
+                            if(myRC.canMove(dir))
+                            {
+                                myRC.setDirection(dir);
+                            }
+                            else{
+                                tryMoveForward();
                             }
                         }
                     }
-                    Direction currDir = myRC.getDirection();
-                    Direction dir = loc.directionTo(minArch);
-                    if(currDir==dir)
+                    else
                     {
                         tryMoveForward();
                     }
-                    else if(dir==Direction.OMNI)
-                    {
-                        //do noting
-                    }
-                    else
-                    {
-                        if(myRC.canMove(dir))
-                        {
-                            myRC.setDirection(dir);
-                        }
-                        else{
-                            tryMoveForward();
-                        }
-                    }
                 }
-                else
+                else if (!myRC.isAttackActive())
                 {
-                    tryMoveForward();
+                    //do an attack if possible
+                    Direction dir = currDir;
+                    do
+                    {
+                        MapLocation l = loc.add(dir);
+                        Robot g = myRC.senseGroundRobotAtLocation(loc.add(dir));
+                        if (g!=null)
+                        {
+                            if (myRC.senseRobotInfo(g).team!=team)
+                            {
+                                //enemy
+                                myRC.attackGround(l);
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            Robot a = myRC.senseAirRobotAtLocation(loc.add(dir));
+                            if (a!=null)
+                            {
+                                if (myRC.senseRobotInfo(a).team!=team)
+                                {
+                                    //enemy
+                                    myRC.attackAir(l);
+                                    break;
+                                }
+                            }
+                        }
+                        dir = dir.rotateRight();
+                    }while (dir!=currDir);
                 }
                 myRC.yield();
 
