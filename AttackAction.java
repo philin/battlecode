43a6ef489c52db943c6c4067e791d33f047768c0
Adam Kidder <thekidder@gmail.com>
2010-01-17 21:32:22 -0500

    added an AttackAction class

---
+++ AttackAction.java
@@ -0,0 +1,37 @@
+package team338;
+
+import battlecode.common.*;
+
+abstract public class AttackAction
+{
+    public AttackAction(double myPriority)
+    {
+        super(myPriority);
+    }
+
+    static class AttackState
+    {
+        RobotLevel level;
+        MapLocation loc;
+    }
+
+    public void run(RobotController rc) throws GameActionException
+    {
+        if(!rc.isAttackActive())
+        {
+            AttackState as = getNextAttackState();
+            switch(as.level)
+            {
+            case IN_AIR:
+                rc.attackAir(as.loc);
+                break;
+            case ON_GROUND:
+                rc.attackGround(as.loc);
+                break;
+            }
+        }
+    }
+
+    abstract protected double getBasePriority();
+    abstract protected AttackState getNextAttackState();
+}
