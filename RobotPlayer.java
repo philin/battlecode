20b58598798c19f2be2bfde80b93d17643ddbf92
Kevin Shih <kevin.j.shih@gmail.com>
2011-01-06 21:54:27 -0500

    building things

--- RobotPlayer.java
+++ RobotPlayer.java
@@ -18,25 +18,162 @@ public class RobotPlayer implements Runnable
 
     public void run()
     {
-        ComponentController [] components = myRC.newComponents();
+        System.out.println(myRC.getTeamResources());
+
         if(myRC.getChassis() != Chassis.BUILDING)
-            runMotor((MovementController)components[0]);
+        {
+            initiateRobotType();
+
+        }
+        else
+        {
+            runBuilding();
+        }
+
     }
 
 
+    public void initiateRobotType()
+    {
+        while (true)
+        {
+            ComponentController [] components = myRC.components();
+            for(int i = 0; i < components.length; ++i)
+            {
+                if(components[i].type() == ComponentType.CONSTRUCTOR)
+                {
+                    runBasicBuilder();
+                }
+            }
+
+            myRC.yield();
+        }
+
+    }
     /*construct a unit of a specified type
       types defined in UnitTypeConstants.java
      */
-    public void buildUnit(BuilderController builder, int type)
+    public void buildUnit(BuilderController builder, int type, MapLocation loc)
     {
-        switch (type)
+        try{
+            if(builder.isActive())
+            {
+                return;
+            }
+            switch (type)
+            {
+            case UnitTypeConstants.BASIC_BUILDER:
+                //create the chassis
+                builder.build(Chassis.LIGHT, loc);
+                //add the components
+                while (true)
+                {
+                    if(builder.isActive() || myRC.getTeamResources() < ComponentType.CONSTRUCTOR.cost)
+                    {
+                        myRC.yield();
+                    }
+                    else
+                    {
+                        builder.build(ComponentType.CONSTRUCTOR, loc, RobotLevel.ON_GROUND);
+                        System.out.println("built component!!!!!!!!!!!!!");
+                        break;
+                    }
+                }
+                break;
+            case UnitTypeConstants.HEAVY_ATTACKER:
+                break;
+            default:
+                break;
+            }
+            System.out.println("built!!!");
+        }
+        catch(Exception ex)
         {
-        case UnitTypeConstants.BASIC_BUILDER:
-            break;
-        case UnitTypeConstants.HEAVY_ATTACKER:
-            break;
-        default:
-            break;
+            ex.printStackTrace();
+        }
+    }
+
+    public void runBuilding()
+    {
+        ComponentController [] components = myRC.components();
+        BuilderController builder = null;
+        for(int i = 0; i < components.length; ++i)
+        {
+            if(components[i].type() == ComponentType.RECYCLER)
+            {
+                builder = (BuilderController)components[i];
+            }
+        }
+        while(true)
+        {
+            System.out.println(myRC.getTeamResources());
+            if(myRC.getTeamResources() > 50)
+            {
+
+                buildUnit(builder,
+                          UnitTypeConstants.BASIC_BUILDER,
+                          myRC.getLocation().add(Direction.SOUTH));
+
+            }
+            myRC.yield();
+        }
+    }
+
+    public void runBasicBuilder()
+    {
+        ComponentController [] components = myRC.components();
+        BuilderController builder = null;
+        MovementController motor = null;
+        for(int i = 0; i < components.length; ++i)
+        {
+            if(components[i].type() == ComponentType.CONSTRUCTOR)
+            {
+                builder = (BuilderController)components[i];
+            }
+            else if(components[i] instanceof MovementController)
+            {
+                motor = (MovementController)components[i];
+            }
+        }
+        System.out.println("identified basic builder");
+        try
+        {
+        while (true)
+        {
+            boolean build = false;
+
+
+            if(myRC.getTeamResources() > 50)
+            {
+                build = true;
+
+
+            }
+            if(!build)
+            {
+
+                if(motor.isActive())
+                {
+                    myRC.yield();
+                }
+                else if (motor.canMove(myRC.getDirection()))
+                {
+                    //System.out.println("about to move");
+                    motor.moveForward();
+                }
+                else
+                {
+                    motor.setDirection(myRC.getDirection().rotateRight());
+                }
+
+
+            }
+            myRC.yield();
+        }
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
         }
     }
 
