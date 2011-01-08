ec4932c4628e524982297a6d4d58d0cd7f6e923c
Jay Yang <jkelleyy@gmail.com>
2011-01-07 22:18:00 -0500

    add IdleTasks

--- Planner.java
+++ Planner.java
@@ -1,9 +1,15 @@
 package team046;
 
+import java.util.LinkedList;
+import battlecode.common.*;
+
 public abstract class Planner{
 
     protected Module navModule,mappingModule,commModule;
-
+    protected LinkedList<IdleTask> tasks = new LinkedList<IdleTask>();
+    protected int maxBytecodes = GameConstants.BYTECODE_LIMIT_BASE;
+    //TODO, find out how many it actually takes
+    private static final int DO_IDLE_OVERHEAD = 100;
     public void init(){
         if(navModule!=null){
             navModule.init(this);
@@ -26,4 +32,19 @@ public abstract class Planner{
         return commModule;
     }
     //Add other module getters here
+
+    public void addIdleTask(IdleTask task){
+        tasks.offer(task);
+    }
+    public void doIdleTask(){
+        while(maxBytecodes-Clock.getBytecodeNum()>500+DO_IDLE_OVERHEAD){
+            IdleTask currTask = tasks.peek();
+            if(currTask==null){
+                break;
+            }
+            if(currTask.executeTask()){
+                tasks.remove();
+            }
+        }
+    }
 }
